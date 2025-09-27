f6d39f88240cb3e5393b54259b576842fba94148
Jkir9 <hyeonsu177@gmail.com>
2025-09-27 14:38:32 +0000

    deleted committed

--- Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
+++ Simulator/obj/Debug/net8.0/Simulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269054b27e5dab6e60d3726be82c7ecbef4a41fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+516a55b27ce375ac152be29b1cebd2e774515330")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
